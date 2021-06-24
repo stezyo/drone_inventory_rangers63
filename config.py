f523bd15243339baadbc5cc892c80511d4b37dc9
stezyo <steveyooon@gmail.com>
2021-06-24 13:42:13 -0700

    depoloy

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config():
     FLASK_APP=os.getenv('FLASK_APP')
     FLASK_ENV=os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET KEY') or 'monichipOO2.'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPOLOY_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #Turn off update messages
